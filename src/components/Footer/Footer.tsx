7ccd5fa94b89876bfa84450ff38eab106edc6fa8
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-05-02 12:55:31 +0530

    footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,29 @@
+import { useContext } from "react";
+import { Facebook, Linkedin, Twitter, Youtube, Instagram } from "react-feather";
+import { WindowContext } from "../../context";
+
 const Footer = () => {
+  const viewportWidth = useContext(WindowContext);
+
+  const socialLinks = [Facebook, Linkedin, Twitter, Youtube, Instagram].map(
+    (Icon) => (
+      <li>
+        <Icon className="transition-all cursor-pointer transform hover:scale-105 hover:text-gray-500" />
+      </li>
+    )
+  );
+
   return (
-    <section className="bg-gray-200">
+    <section className={`bg-gray-200 ${viewportWidth < 640 ? "mt-10" : ""}`}>
       <div className="text-center py-6">
-        <h3 className=" text-gray-800 text-3xl font-medium">Found Out More</h3>
-        <ul className="text-gray-500">ICONS</ul>
+        <h3 className=" text-gray-600 text-3xl font-semibold">
+          Found Out More
+        </h3>
+        <ul className="text-gray-400 mt-3 flex space-x-10 justify-center">
+          {socialLinks}
+        </ul>
       </div>
-      <div className="text-center">
+      <div className="text-center bg-white">
         Made By{" "}
         <a
           className="font-medium underline"
@@ -13,7 +31,7 @@ const Footer = () => {
         >
           Harsh
         </a>{" "}
-        With ❤❤
+        With <span className="text-red-600">❤❤</span>
       </div>
     </section>
   );
